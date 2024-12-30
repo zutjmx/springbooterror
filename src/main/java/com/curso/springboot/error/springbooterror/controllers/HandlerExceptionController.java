364158b92838e6a39fe95cc4a29cca8f094eb038
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-12-30 17:52:05 -0600

    Modificaciones 30/12/2024 17:51

--- src/main/java/com/curso/springboot/error/springbooterror/controllers/HandlerExceptionController.java
+++ src/main/java/com/curso/springboot/error/springbooterror/controllers/HandlerExceptionController.java
@@ -1,15 +1,25 @@
 package com.curso.springboot.error.springbooterror.controllers;
 
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.curso.springboot.error.springbooterror.models.Error;
 
 @RestControllerAdvice
 public class HandlerExceptionController {
     
     @ExceptionHandler({ArithmeticException.class})
-    public ResponseEntity<?> divisionPorCero(Exception ex) {
-        return ResponseEntity.internalServerError().body("Error: " + ex.getMessage() + " - " + ex.getCause());
+    public ResponseEntity<Error> divisionPorCero(Exception ex) {
+        Error error = new Error();
+        error.setFecha(new Date());
+        error.setMensaje("Error de división por cero");
+        error.setMensaje(ex.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return ResponseEntity.internalServerError().body(error);
     }
 
 }
