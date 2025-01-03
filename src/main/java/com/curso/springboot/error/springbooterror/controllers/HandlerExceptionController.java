5d02539c1cc3f868b46ace2856ef1a556b291066
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-01-03 13:08:53 -0600

    Personalizar error 404 NOT FOUND

--- src/main/java/com/curso/springboot/error/springbooterror/controllers/HandlerExceptionController.java
+++ src/main/java/com/curso/springboot/error/springbooterror/controllers/HandlerExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.curso.springboot.error.springbooterror.models.Error;
 
@@ -21,6 +22,16 @@ public class HandlerExceptionController {
         error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         //return ResponseEntity.internalServerError().body(error);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> noEncontrado(Exception ex) {
+        Error error = new Error();
+        error.setFecha(new Date());
+        error.setMensaje("Ruta de API no encontrada");
+        error.setError(ex.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
 }
