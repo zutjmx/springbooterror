f9e41ac36f99bd8e0275f7f769fc21bee9738d5a
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-01-06 12:36:08 -0600

    Modificaciones 06/01/2025 12:35

--- src/main/java/com/curso/springboot/error/springbooterror/controllers/HandlerExceptionController.java
+++ src/main/java/com/curso/springboot/error/springbooterror/controllers/HandlerExceptionController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -27,6 +28,7 @@ public class HandlerExceptionController {
     }
 
     @ExceptionHandler({NumberFormatException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String, String> formatoNumero(Exception ex) {
         Map<String, String> error = new HashMap<>();
         error.put("mensaje", "Error de formato de número");
